f18ea83a5992b4573aece87e7a7a2b9cc3acdf67
Guel-x <ml2338668@gmail.com>
2024-03-27 15:14:02 -0300

    Primeiro commit aula 3

---
+++ SistemaFinanceiro/Notificacoes/Notifica.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Notificacoes
+{
+    public class Notifica
+    {
+
+        public Notifica()
+        {
+            notificacoes = new List<Notifica>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifica> notificacoes { get; set; }
+
+        public bool ValidarPropriedadeString( string valor, string nomePropeiedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropeiedade))
+            {
+                notificacoes.Add(new Notifica
+                {
+                    mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropeiedade,
+                });
+
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidarPropriedadeInt(int valor, string nomePropeiedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropeiedade))
+            {
+                notificacoes.Add(new Notifica
+                {
+                    mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropeiedade,
+                });
+
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
